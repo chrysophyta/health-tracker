d0f3c9db22d4bf32f39a5ac2b5b3df66bd78b4c1
Vivian <chrysophyta@gmail.com>
2018-10-04 23:45:10 +0800

    add d3

---
+++ src/Chart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+class Chart extends Component {
+  render() {
+    return (
+      <div className="Chart">
+        <svg />
+      </div>
+    );
+  }
+}
+
+export default App;
