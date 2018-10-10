7a6b91298ff57c9b0207ff2b20943ba7865bf16a
Vivian <chrysophyta@gmail.com>
2018-10-10 23:49:02 +0800

    show a line but not as expected

--- src/Chart.js
+++ src/Chart.js
@@ -1,60 +1,52 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
+// Use the margin convention practice
+const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+const width = window.innerWidth - margin.left - margin.right; // Use the window's width
+const height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
+
 class Chart extends Component {
   state = {
-    data: null
+    weights: null,
+    temps: null
   };
-  componentDidMount() {
-    this.createBarChart();
-  }
-  componentDidUpdate() {
-    this.createBarChart();
-  }
 
-  createBarChart = () => {};
+  n = this.props.data.length;
+  xScale = d3.scaleLinear().range([0, width]); // output
+
+  // Y scale will use the randomly generate number
+  yScale = d3.scaleLinear().range([height, 0]); // output
+
+  lineGenerator = d3.line();
+
   componentWillReceiveProps(nextProps) {
-    // Use the margin convention practice
-    const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-    const width = window.innerWidth - margin.left - margin.right; // Use the window's width
-    const height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
+    const { data } = nextProps;
+    if (!data) return;
 
-    // n of datapoints
-    var n = this.props.data.length;
+    const timeDomain = d3.extent(data, (d, i) => i);
+    const tempMax = d3.max(data, d => d.temp);
+    this.xScale.domain(timeDomain);
+    this.yScale.domain([0, tempMax]);
 
-    // X scale will use the index of our data
-    var xScale = d3
-      .scaleLinear()
-      .domain([0, n - 1]) // input
-      .range([0, width]); // output
+    this.lineGenerator.x((d, i) => this.xScale(i));
+    this.lineGenerator.y(d => this.xScale(d.temp));
+    const temps = this.lineGenerator(data);
 
-    // Y scale will use the randomly generate number
-    var yScale = d3
-      .scaleLinear()
-      .domain([0, 1]) // input
-      .range([height, 0]); // output
-
-    var line = d3
-      .line()
-      .x(function(d, i) {
-        return xScale(i);
-      }) // set the x values for the line generator
-      .y(function(d) {
-        return yScale(d.y);
-      }) // set the y values for the line generator
-      .curve(d3.curveMonotoneX); // apply smoothing to the line
-    console.log(line);
-    const dataline = line(nextProps.data);
-    this.setState({ data: dataline });
+    this.lineGenerator.y(d => this.yScale(d.weight));
+    const weights = this.lineGenerator(data);
     // An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-    var dataset = d3.range(n).map(function(d) {
-      return { y: d3.randomUniform(1)() };
-    });
+
+    this.setState({ weights, temps });
   }
   render() {
+    console.log(this.state.temps);
     return (
       <div className="Chart">
-        <svg width={500} height={500} />
+        <svg width={width} height={height}>
+          <path d={this.state.temps} fill="none" stroke={'#000000'} />
+          <path d={this.state.weights} fill="none" stroke={'#000000'} />
+        </svg>
       </div>
     );
   }
